783c081ff77cda517c47fb51761c463c83b4980d
jhdavey <jhdavey5182@gmail.com>
2023-05-18 12:16:26 -0400

    Add department and and role functions complete

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# employeecms
+# Employee CMS
+
+![License](https://img.shields.io/static/v1?label=license&message=MIT&color=${badge.color})
+
+  ## Description
+  
+This app allows users to manage their employee database.
+  
+## Table of Contents
+  
+* [Installation](#Installation)
+  
+* [Usage](#Usage)
+
+* [Technologies](#Technologies-Used)
+  
+* [Contributing](#Contributing)
+  
+* [Tests](#Tests)
+  
+* [Questions](#Questions)
+  
+* [License](#License)
+  
+## Installation
+  
+To install, pull down the files from the repo. Then run `npm install` in the directory you would like to work in. Next you will need to create your database in MySql. You can do this by opening Mysql in your CLI and running the command `SOURCE schema.sql`. This will create your employee database along with departments, roles, and employees tables. IF you would like to seed the database, run `SOURCE seeds.sql`.
+
+## Usage
+  
+Once you have installed the app, right click on one of your files in the directory you're working in. Click "Open in Integrated Terminal". Then in your command line, type `npm start`. You will be prompted with the employee database menu.
+
+Here you will see options to view departments, roles, and employees, add departments, roles, and employees, as well as update employee records. 
+  
+## Technologies Used
+
+<div style="display: inline_block"><br>
+  <img height="40" align="center" alt="JavaScript" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ <img height="40" align="center" alt="Node.js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg">
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ <img height="40" align="center" alt ="mysql" height ="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg">
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+
+## Contributing
+  
+Please let me know if you'd like to contribute to my project!
+  
+## Tests
+  
+If you run into any errors or bugs while using this app, please contact me.
+  
+## Questions
+  
+Have more questions? Head over to my github profile or email me:
+  
+[Checkout my GitHub](www.github.com/jhdavey)
+  
+[Email me!](hdwebdevelopment@gmail.com)
+  
+## License
+  
+License: MIT - see the [License](https://choosealicense.com/licenses/mit/) page.
+
+  
--- server.js
+++ server.js
@@ -1,41 +1,325 @@
-const express = require('express');
-// Import and require mysql2
+//Import dependencies
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+//Inquirer questions array
+const menu =
+    [
+        {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'EXIT'],
+        name: 'action'
+        },
+];
 
 // Connect to database
-const db = mysql.createConnection(
-  {
+const db = mysql.createConnection({
     host: 'localhost',
-    // MySQL Username
     user: 'root',
-    // TODO: Add MySQL Password
-    password: '',
-    database: 'books_db'
+    password: 'password',
+    database: 'employee_db'
   },
-  console.log(`Connected to the books_db database.`)
 );
 
-// Query database using COUNT() and GROUP BY
-db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-  console.log(results);
+// connects to sql database
+db.connect(function(err){
+
+  if (err) throw err;
+
+  //Launch Employee Database Menu
+  launchMenu();
+
 });
 
-// Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
-db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-  console.log(results);
-});
+//Run each function based on user action
+function launchMenu() {
 
-app.use((req, res) => {
-  res.status(404).end();
-});
+  // prompt user actions using inquirer 
+  inquirer.prompt(menu)
+    .then(function(answer) {
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+      // execute function viewAll if user selection is "View employees"
+      if(answer.action == "View All Employees") {
+          
+          viewAllEmployees();
+      
+      // execute function viewDept if user selection is "View departments"
+      }else if(answer.action == "View All Departments") {
+
+          viewDept();
+
+      // execute function viewRoles if user selection is "View roles"
+      }else if(answer.action == "View All Roles") {
+
+          viewRoles();
+
+      // execute function addEmployee if user selection is "Add employee"
+      }else if(answer.action == "Add An Employee") {
+
+          addEmployee();
+          
+      // execute function addDept if user selection is "Add department"
+      }else if(answer.action == "Add A Department") {
+
+          addDept();
+      
+      // execute function addRole if user selection is "Add roles"
+      }else if(answer.action == "Add A Role") {
+
+          addRole();
+
+      // execute function addRole if user selection is "Add roles"
+      }else if(answer.action == "Edit employee") {
+
+          updateEmployee();
+
+      // execute function addRole if user selection is "Add roles"
+      }else if(answer.action == "Remove employee") {
+
+          deleteEmployee();
+
+      // execute function EXIT if user selection is "EXIT"
+      }else if(answer.action == "EXIT") {
+
+          exit();
+
+      }
+  })
+};
+
+//Functions to return responses based on user action
+function viewAllEmployees() {
+
+  let query = "SELECT employees.e_id, employees.first_name, employees.last_name, roles.title AS title, departments.department_name AS department, roles.salary AS salary FROM employees JOIN roles ON employees.role_id = roles.r_id JOIN departments ON roles.department_id = departments.d_id";
+
+  // connect to mySQL with query to view all employees
+  db.query(query, function(err, res) {
+
+      if (err) throw err;
+
+      // show All Employees table
+      console.info('');
+      console.table(res); 
+
+      // prompt user for next action
+      launchMenu();
+  });
+};
+
+function viewDept() {
+
+    let query = "SELECT * FROM departments";
+  
+    // connect to mySQL with query to view all departments
+    db.query(query, function(err, res) {
+  
+        if (err) throw err;
+  
+        // show All departments
+        console.info('');
+        console.table(res); 
+  
+        // prompt user for next action
+        launchMenu();
+    });
+  };
+
+  function viewRoles() {
+
+    let query = "SELECT * FROM roles";
+  
+    // connect to mySQL with query to view all roles
+    db.query(query, function(err, res) {
+  
+        if (err) throw err;
+  
+        // show All roles table
+        console.info('');
+
+        console.table(res); 
+  
+        // prompt user for next action
+        launchMenu();
+    });
+};
+
+//Function to add new employee 
+async function addEmployee() {
+
+  //Create an array with current employees for manager options
+  let employeeQuery = 'SELECT first_name, last_name FROM employees';
+
+  let employeeArray = [];
+
+    db.query(employeeQuery, function(err, res) {
+
+      if (err) throw err;
+
+      employeeArray = res.map(function (obj) {
+
+      var employeeNames = `${obj.first_name} ${obj.last_name}`;
+
+      employeeArray.push(employeeNames);
+
+      return employeeArray;
+
+      });
+    });
+
+  const addEmployeeQuestions =
+  [
+      {
+      type: 'input',
+      message: 'First name of new employee:',
+      name: 'firstName'
+      },
+      {
+      type: 'input',
+      message: 'Last name of new employee:',
+      name: 'lastName'
+      },
+      {
+      type: 'list',
+      message: 'New employee role:',
+      choices: ['Account Executive', 'Sr Account Executive', 'Sales Director', 'HR Coordinator', 'HR Director', 'Jr Developer', 'Sr Developer', 'Programming Director', 'IT Project Manager', 'IT Project Director', 'Chief Executive Officer', 'Chief Operating Officer', 'Chief Financial Officer'],
+      name: 'newRole'
+      },
+      {
+      type: 'list',
+      message: 'New employee\'s manager:',
+      choices: employeeArray,
+      name: 'employeeManager'
+      },
+  ];
+
+  // prompt user for new employee information
+  await inquirer.prompt(addEmployeeQuestions)
+    .then(function(answer) {
+
+      //Get manager's first name to pull Id before inserting as new employee's manager_id
+      let firstNameSlice = answer.employeeManager.replace(/ .*/,'');
+
+      //TODO - FIX GETTING ACTUAL MANAGER ID
+      let mngrName = `SELECT e_id FROM employees WHERE first_name = "${firstNameSlice}"`;
+
+      var mId = 1;
+
+      // db.query(mngrName, function(err, res) {
+
+      //   if (err) throw err;
+
+      //   mId = res.e_id;
+
+      // });
+
+      //TODO - FIX INSERTING ACTUAL MANAGER ID
+      let roleId = `SELECT r_id FROM roles WHERE title = "${answer.newRole}"`;
+
+      var rId = 1;
+
+      // db.query(roleId, function(err, res) {
+
+      //   if (err) throw err;
+
+      //   rId = res.r_id;
+  
+      // });
+
+      //Insert new employee info into employees table
+      let insertEmployeeQuery = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+
+      let eValues = [answer.firstName, answer.lastName, rId, mId];
+
+      // connect to mySQL with insert employee query
+      db.query(insertEmployeeQuery, eValues, function(err, res) {
+
+        if (err) throw err;
+
+        console.info(`New employee added to employees table! ${res}`)
+
+        // prompt user for next action
+        launchMenu();
+    });
+  });
+};
+
+//Create New Department
+async function addDept() {
+
+  const addDepartmentQuestions =
+  [
+      {
+      type: 'input',
+      message: 'Name of new Department:',
+      name: 'deptName'
+      },
+  ];
+
+  // prompt user for new department information
+  await inquirer.prompt(addDepartmentQuestions)
+    .then(function(answer) {
+
+      //Insert new department info into departments table
+      let insertDeptQuery = `INSERT INTO departments (department_name) VALUES (?)`;
+
+      // connect to mySQL with insert query
+      db.query(insertDeptQuery, answer.deptName, function(err, res) {
+
+        if (err) throw err;
+
+        console.info(`New department added to departments table! ${res}`)
+
+        // prompt user for next action
+        launchMenu();
+    });
+  });
+};
+
+//Create New Role
+async function addRole() {
+
+  const addRoleQuestions =
+  [
+      {
+      type: 'input',
+      message: 'Name of new Role:',
+      name: 'roleTitle'
+      },
+      {
+        type: 'input',
+        message: 'Salary of new Role (No symbols, round numbers only):',
+        name: 'roleSalary'
+        },
+        {
+          type: 'list',
+          message: 'Assign Role to department:',
+          choices: ['Sales', 'Human Resources', 'Engineering', 'IT', 'Finance', 'Executive'],
+          name: 'departmentName'
+          },
+  ];
+
+  // prompt user for new role information
+  await inquirer.prompt(addRoleQuestions)
+    .then(function(answer) {
+
+      //Insert new role info into roles table
+      let insertRoleQuery = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+
+      //TODO - FIX INSERTING ACTUAL MANAGER ID 
+      let dId = 1;
+
+      let rValues = [answer.roleTitle, answer.roleSalary, dId];
+
+      // connect to mySQL with insert query
+      db.query(insertRoleQuery, rValues, function(err, res) {
+
+        if (err) throw err;
+
+        console.info(`New role added to roles table! ${res}`)
+
+        // prompt user for next action
+        launchMenu();
+    });
+  });
+};
